e5d30d2b30a19283ae07604f043d93f02d35391d
quicknightbird <eliasriv73@gmail.com>
2022-05-23 03:57:52 +0300

    add server and client

---
+++ client/src/client.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class client {
+    public static void main(String[] args) {
+        try(Socket socket = new Socket("127.0.0.1",8000) ){
+
+                try (
+                        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                ){
+                    System.out.println("Connected");
+                    String request = "BLO";
+                    writer.write(request);
+                    writer.newLine();
+                    writer.flush();
+
+                    String response = reader.readLine();
+                    System.out.println(response);
+                }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ server/src/server.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+    public static void main(String[] args) {
+
+        try(ServerSocket server = new ServerSocket(8000) ){
+            System.out.println("Server started");
+            while (true)
+                try (
+                    Socket socket = server.accept();
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                ){
+                    String request = reader.readLine();
+                    System.out.println(request);
+                    String response = "Hello from server" + request.length();
+                    System.out.println(response);
+                    writer.write(response);
+                    writer.newLine();
+                    writer.flush();
+                }
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
